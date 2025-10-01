743e76e57b3b42492df0494538540fa261de3185
g4rrucho <guilhermegarrucho@gmail.com>
2025-10-01 11:38:21 +0100

    feat: add header component for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 
 import PokemonCard from '@/components/PokemonCard';
 import PokemonList from '@/components/PokemonList';
+import Header from '@/components/Layout/Header';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
+      <Header />
       <Routes>
         <Route path="/" Component={PokemonList} />
         <Route path="/pokemon/:id" Component={PokemonCard} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
-    </div>
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
 import App from './App.tsx';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient.ts';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter } from 'react-router-dom';
 import { PokedexProvider } from '@/contexts/PokedexContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
